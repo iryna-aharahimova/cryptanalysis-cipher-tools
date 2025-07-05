6dff967590cb12e42525be9d6eebc9e8d7373877
Iryna Aharahimova <IrynaA@neogames.com>
2025-07-05 14:36:28 +0300

    Add unit tests for Encode and Decode service classes as examples

---
+++ src/test/java/com/cryptoanalyzer/aharahimova/utils/TestUtils.java
@@ -0,0 +1,13 @@
+package com.cryptoanalyzer.aharahimova.utils;
+
+import java.util.Random;
+
+public class TestUtils {
+    private static Random random = new Random();
+    private static final int MIN_KEY = 1;
+    private static final int MAX_KEY = 70;
+
+    public static int getRandomKey() {
+        return random.nextInt(MIN_KEY, MAX_KEY); // 1 до 70 включительно
+    }
+}
